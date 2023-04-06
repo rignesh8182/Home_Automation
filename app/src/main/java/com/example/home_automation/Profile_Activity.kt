a2d7e528654af6a759424aee95bd59ddd0d6d479
rignesh8182 <85547043+rignesh8182@users.noreply.github.com>
2023-04-06 17:35:00 +0530

    commit

---
+++ app/src/main/java/com/example/home_automation/Profile_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.home_automation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Profile_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+    }
+}
